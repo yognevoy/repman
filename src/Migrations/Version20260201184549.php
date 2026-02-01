8d5bc9ce1bae09a230b4fde5d1120eef512fd7df
yognevoy <ya.ognevoy@gmail.com>
2026-02-01 21:52:40 +0300

    feat(database): rename lock columns migration

---
+++ src/Migrations/Version20260201184549.php
@@ -0,0 +1,33 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Buddy\Repman\Migrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20260201184549 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE organization_package RENAME COLUMN locked_version TO max_version');
+        $this->addSql('ALTER TABLE organization_package RENAME COLUMN locked_until TO max_release_date');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE organization_package RENAME COLUMN max_version TO locked_version');
+        $this->addSql('ALTER TABLE organization_package RENAME COLUMN max_release_date TO locked_until');
+    }
+}
